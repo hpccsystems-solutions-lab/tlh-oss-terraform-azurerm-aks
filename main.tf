429e3bb6c1a0168b06003bf7ec7e7e0217e2d3d2
Tim Miller <tim.miller@risk.lexisnexis.com>
2021-03-28 11:16:34 -0400

    cleanup

--- main.tf
+++ main.tf
@@ -1,29 +1,31 @@
 module "nodes" {
   source = "./modules/nodes"
 
-  cluster_name        = local.cluster_name
-  network_plugin      = var.network_plugin
-  subnets             = var.subnets
+  cluster_name   = local.cluster_name
+  network_plugin = var.network_plugin
+  subnets        = var.subnets
 
-  node_pool_defaults  = var.node_pool_defaults
-  node_pool_taints    = var.node_pool_taints
-  node_pool_tags      = merge(var.tags, var.node_pool_tags)
-  node_pools          = var.node_pools
+  node_pool_defaults = var.node_pool_defaults
+  node_pool_taints   = var.node_pool_taints
+  node_pool_tags     = merge(var.tags, var.node_pool_tags)
+  node_pools         = var.node_pools
 }
 
 module "kubernetes" {
   source = "github.com/Azure-Terraform/terraform-azurerm-kubernetes.git?ref=v3.0.2"
 
-  location                 = var.location
-  tags                     = var.tags
-  resource_group_name      = var.resource_group_name
-  
+  location            = var.location
+  tags                = var.tags
+  resource_group_name = var.resource_group_name
+
   cluster_name = local.cluster_name
   dns_prefix   = local.cluster_name
 
+  kubernetes_version = local.cluster_version
+
   network_plugin = var.network_plugin
 
-  node_pool_subnets  = var.subnets
-  node_pools         = module.nodes.node_pools
-  default_node_pool  = module.nodes.default_node_pool
+  node_pool_subnets = var.subnets
+  node_pools        = module.nodes.node_pools
+  default_node_pool = module.nodes.default_node_pool
 }
