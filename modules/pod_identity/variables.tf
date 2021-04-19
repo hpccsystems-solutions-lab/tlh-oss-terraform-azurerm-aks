a70b6860abc73ee282b6cc3bb73542dc0493f525
Tim Miller <tim.miller@risk.lexisnexis.com>
2021-04-19 16:52:16 -0400

    add pod_identity submodule

---
+++ modules/pod_identity/variables.tf
@@ -0,0 +1,20 @@
+variable "aks_identity" {
+  description = "Kubelet identity client_id."
+  type        = string
+}
+
+variable "aks_node_resource_group" {
+  description = "resource group created by AKS"
+  type        = string
+}
+
+variable "network_plugin" {
+  description = "Kubernetes Network Plugin (kubenet or azure)"
+  type        = string
+  default     = "kubenet"
+
+  validation {
+    condition     = contains(["kubenet", "azure"], var.network_plugin)
+    error_message = "Network plugin must be kubenet or azure."
+  }
+}
