a58c2ae76ece90dbaf785754a1537ab93c865a17
Tim Miller <tim.miller@risk.lexisnexis.com>
2021-05-17 14:50:27 -0400

    remove kubenet2 example

--- examples/kubenet/variables.tf
+++ examples/kubenet/variables.tf
@@ -4,16 +4,11 @@ variable "external_dns_zones" {
     names               = list(string)
     resource_group_name = string
   })
-  default = null
 }
 
 variable "cert_manager_dns_zones" {
   description = "The name and resource group of the DNS zone associated with your Azure subscription"
-  type = object({
-    names = list(string)
-    resource_group_name = string
-  })
-  default = null
+  type = map(string)
 }
 
 variable "rbac_admin_object_ids" {
