89286f329541378ac70f9a6e121ba4f59c8c478c
Tim Miller <millti01@risk.regn.net>
2021-09-11 20:41:01 -0400

    update examples to match changed output name (#163)
    Co-authored-by: Tim Miller <tim.miller@risk.lexisnexis.com>

--- examples/AzureCNI/main.tf
+++ examples/AzureCNI/main.tf
@@ -179,5 +179,5 @@ module "aks" {
 }
 
 output "aks_login" {
-  value = "az aks get-credentials --name ${module.aks.aks_cluster_name} --resource-group ${module.resource_group.name}"
+  value = "az aks get-credentials --name ${module.aks.cluster_name} --resource-group ${module.resource_group.name}"
 }
