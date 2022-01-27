8864a31f102235e755e18e4d78304707bfb1cbe8
dutsmiller@gmail.com <tim.miller@risk.lexisnexis.com>
2022-01-27 14:50:22 -0500

    require variable, checked in parent

--- modules/core-config/modules/storage-classes/variables.tf
+++ modules/core-config/modules/storage-classes/variables.tf
@@ -1,10 +1,4 @@
 variable "cluster_version" {
   description = "The Kubernetes minor version to use for the AKS cluster."
   type        = string
-  default     = "1.21"
-
-  validation {
-    condition     = contains(["1.21", "1.20", "1.19"], var.cluster_version)
-    error_message = "This module only supports AKS versions 1.21, 1.20 & 1.19."
-  }
 }
