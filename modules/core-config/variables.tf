92714718d5742cb85a0a389c4afd58ca7e38580e
dutsmiller@gmail.com <tim.miller@risk.lexisnexis.com>
2022-01-27 15:05:00 -0500

    remove default, use validation from parent

--- modules/core-config/variables.tf
+++ modules/core-config/variables.tf
@@ -51,12 +51,6 @@ variable "cluster_id" {
 variable "cluster_version" {
   description = "The Kubernetes minor version to use for the AKS cluster."
   type        = string
-  default     = "1.21"
-
-  validation {
-    condition     = contains(["1.21", "1.20", "1.19"], var.cluster_version)
-    error_message = "This module only supports AKS versions 1.21, 1.20 & 1.19."
-  }
 }
 
 variable "aks_identity" {
