60b2bc5094ed686e892699baaba862792e0bcee5
Prikesh Patel <60138114+prikesh-patel@users.noreply.github.com>
2022-10-17 16:09:53 +0100

    bug: Remove support for the Helm provider v2.7.1 (#713)

--- versions.tf
+++ versions.tf
@@ -8,7 +8,7 @@ terraform {
     }
     helm = {
       source  = "hashicorp/helm"
-      version = ">= 2.5.1, != 2.7.0"
+      version = ">= 2.5.1, != 2.7.0, != 2.7.1"
     }
     kubectl = {
       source  = "gavinbunney/kubectl"
